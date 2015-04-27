2ce6f20dc04c7ffe7fcaad83149cdf618286d3e1
古堂　淳也　 <junya_furudo@jfurudo.local>
2015-04-27 19:22:30 +0900

    added function for open and close backet operater.

--- brainfuck-mode.el
+++ brainfuck-mode.el
@@ -5,29 +5,73 @@
   (let ((pointer 0)
         (memory (make-vector brainfuck-mode-memory-size 0))
         (program-counter 0)
-        (output-buffer (get-buffer-create "output")))
+        (output-buffer (get-buffer-create "output"))
+        (result ""))
     (pop-to-buffer output-buffer)
-    (let ((char (substring source program-counter (+ program-counter 1))))
-      (insert char)
-      (cond
-       ((string= char "+")
-        (aset memory program-counter (+ (aref memory program-counter) 1)))
-       (t (insert "hoge"))))
-;;    (message (number-to-string pointer))
+    (let ((i 0))
+      (while (< program-counter (length source))
+        (let ((operater (substring source program-counter (+ program-counter 1))))
+          ;; (brainfuck-execlude-operater pointer memory operater)
+          (cond
+           ((string= operater "+")
+            (aset memory pointer (+ (aref memory pointer) 1)))
+           ((string= operater "-")
+            (aset memory pointer (- (aref memory pointer) 1)))
+           ((string= operater ">")
+            (incf pointer))
+           ((string= operater "<")
+            (decf pointer))
+           ((string= operater ".")
+            (setq result (concat result (format "%c" (aref memory pointer)))))
+           ((string= operater "[")
+            (if (= (aref memory pointer) 0)
+                (jump-to-close-bracket)))
+           ((string= operater "]")
+            (if (not (= (aref memory pointer) 0))
+                (jump-to-open-bracket)))))
+        (incf program-counter)
+        (insert (number-to-string program-counter))
+        (incf i)))
     (insert (brainfuck-mode-show pointer memory))))
 
-(string= "+" "+")
+; (brainfuck-mode-show 0 (make-vector brainfuck-mode-memory-size 0))
 
-(interpret-Brainfuck "++")
-(message (number-to-string brainfuck-mode-memory-size))
-(make-vector 10 0)
+(defun jump-to-close-bracket ()
+  (catch 'found-open-bracket
+    (while (< program-counter (length source))
+      (if (= ?\] (aref source program-counter))
+          (throw 'found-open-bracket t)
+        (incf program-counter)))))
 
-(brainfuck-mode-show 0 (make-vector brainfuck-mode-memory-size 0))
+(defun jump-to-open-bracket ()
+  (catch 'found-close-bracket
+    (while (> program-counter -1)
+      (if (= ?\[ (aref source program-counter))
+          (throw 'found-close-bracket t)
+        (decf program-counter)))))
+
+(defun brainfuck-execlude-operater (pointer memory operater)
+  (cond
+   ((string= operater "+")
+    (aset memory pointer (+ (aref memory pointer) 1)))
+   ((string= operater "-")
+    (aset memory pointer (- (aref memory pointer) 1)))
+   ((string= operater ">")
+    (incf pointer))
+   ((string= operater "<")
+    (decf pointer))
+   (t (insert "not inplemented"))))
+
+(defun hoge ()
+    (let ((pointer 0) (memory (make-vector brainfuck-mode-memory-size 0)))
+      (brainfuck-execlude-operater pointer memory ">")))
+
+(brainfuck-execlude-operater '0 (make-vector brainfuck-mode-memory-size 0) ">")
 
 ;; show pointer and memory.
 (defun brainfuck-mode-show (pointer memory)
   "Show memory state."
-  (let ((pointer-line "pointer: ") (memory-line "address"))
+  (let ((pointer-line "pointer: ") (memory-line "address: ") (result-line "result: "))
     (setq pointer-line (concat pointer-line (number-to-string pointer)))
     (let ((i 0))
       (while (< i brainfuck-mode-memory-size)
@@ -35,7 +79,11 @@
               (concat memory-line " "
                       (number-to-string (aref memory i))))
         (setq i (+ i 1))))
-    (concat pointer-line "\n" memory-line)))
+    (concat pointer-line "\n" memory-line "\n" result-line result)))
 
 (1+ brainfuck-mode-memory-size)
-(make-vector 0 0)
+
+(interpret-brainfuck "++++[>++++<-]+.")
+
+(provide 'brainfuck-mode)
+
