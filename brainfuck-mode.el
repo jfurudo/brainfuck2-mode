067906f9daf2e9875e521230475e7975f303ac20
古堂　淳也　 <junya_furudo@jfurudo.local>
2015-04-30 23:26:37 +0900

    divide function and make keymap

--- brainfuck-mode.el
+++ brainfuck-mode.el
@@ -1,11 +1,23 @@
 (setq load-path (cons "~/Development/Emacs-Lisp/brainfuck-mode" load-path))
 (require 'brainfuck-output-mode)
 
+(defvar brainfuck-mode-map (make-keymap))
+
+(define-key brainfuck-mode-map "\C-xe" 'brainfuck-eval-buffer)
+
+(defun brainfuck-mode ()
+  (interactive)
+  (setq major-mode 'brainfuck-mode)
+  (setq mode-name "brainfuck-mode")
+  (use-local-map brainfuck-mode-map))
+
+
 (defface brainfuck-face-output-focused '((t . (:background "red" :foreground "black"))) "Face for the tile 2" :group 'brainfuck-mode-faces)
 
-(setq brainfuck-mode-memory-size 10)
+;; TODO: Replace to buffer local valiable.
+(setq brainfuck-mode-memory-size 100)
 
-(defun interpret-brainfuck (source interval)
+(defun brainfuck-interpret (source step-execute)
   "Interpret brainfuck program."
   (let ((pointer 0)
         (memory (make-vector brainfuck-mode-memory-size 0))
@@ -16,29 +28,18 @@
     (brainfuck-output-mode)
     (let ((i 0))
       (while (< program-counter (length source))
-        (let ((operater (substring source program-counter (+ program-counter 1))))
-          (cond
-           ((string= operater "+")
-            (aset memory pointer (+ (aref memory pointer) 1)))
-           ((string= operater "-")
-            (aset memory pointer (- (aref memory pointer) 1)))
-           ((string= operater ">")
-            (incf pointer))
-           ((string= operater "<")
-            (decf pointer))
-           ((string= operater ".")
-            (setq result (concat result (format "%c" (aref memory pointer)))))
-           ((string= operater "[")
-            (if (= (aref memory pointer) 0)
-                (jump-to-close-bracket)))
-           ((string= operater "]")
-            (if (not (= (aref memory pointer) 0))
-                (jump-to-open-bracket)))))
-        (brainfuck-mode-show)
-        (sit-for interval)
+        (let ((command (substring source program-counter (+ program-counter 1))))
+          (brainfuck-execute-command)
+          )
+        (if step-execute
+            (progn 
+              (brainfuck-mode-show)
+              (message "(N)ext / (E)nd step exexute")
+              (let ((input (read-char)))
+                (if (= input ?e)
+                    (setq step-execute nil)))))
         (incf program-counter)
-        (incf i)))
-    ))
+        (incf i)))))
 
 (defun jump-to-close-bracket ()
   (catch 'found-open-bracket
@@ -53,7 +54,32 @@
       (if (= ?\[ (aref source program-counter))
           (throw 'found-close-bracket t)
         (decf program-counter)))))
-(defun brainfuck-execlude-operater ())
+
+(defun brainfuck-execute-command ()
+  "Execute a command."
+  (cond
+   ((string= command "+")
+    (aset memory pointer (+ (aref memory pointer) 1)))
+   ((string= command "-")
+    (aset memory pointer (- (aref memory pointer) 1)))
+   ((string= command ">")
+    (incf pointer))
+   ((string= command "<")
+    (decf pointer))
+   ((string= command ".")
+    (setq result (concat result (format "%c" (aref memory pointer)))))
+   ((string= command ",")
+    (aset memory pointer (read-char)))
+   ((string= command "[")
+    (if (= (aref memory pointer) 0)
+        (jump-to-close-bracket)))
+   ((string= command "]")
+    (if (not (= (aref memory pointer) 0))
+        (jump-to-open-bracket)))
+   ;; Command "#" is braek point.
+   ((string= command "#")
+    (setq step-execute t)))
+  )
 
 (defun brainfuck-mode-show ()
   "Show memory state."
@@ -66,6 +92,8 @@
     (setq pointer-line (concat pointer-line (number-to-string pointer)))
     (let ((i 0))
       (while (< i brainfuck-mode-memory-size)
+        ;; (if (= (% i 16) 0)
+        ;;     (setq memory-line (concat memory-line "\n         ")))
         (setq memory-line
               (concat memory-line " "
                       (format "%02x" (aref memory i))))
@@ -74,6 +102,8 @@
     (put-text-property (brainfuck-source-point ?s) (brainfuck-source-point ?e) 'face 'brainfuck-face-output-focused source-line)
     (put-text-property (brainfuck-memory-point ?s) (brainfuck-memory-point ?e) 'face 'brainfuck-face-output-focused memory-line)
     (insert (concat source-line "\n" pointer-line "\n" memory-line "\n" result-line result))))
+
+(% 0 16)
 
 (defun brainfuck-source-point (start-or-end)
   (if (= start-or-end ?s)
@@ -85,6 +115,9 @@
       (+ 10 (* 3 pointer))
     (+ 12 (* 3 pointer))))
 
-(interpret-brainfuck "+++++++++[>++++++++<-]>.<+++++++++[>+++<-]>++.+++++++..+++.<+++++++++[>-------<-]>----.<+++++++++[>-<-]>---.<+++++++++[>++++++<-]>+.<+++++++++[>++<-]>++++++.+++.------.--------." 0.05)
+;; (brainfuck-interpret "+++++++++[>++++++++<-]>.<+++++++++[>+++<-]>++.+++++++..+++.<+++++++++[>-------<-]>----.<+++++++++[>-<-]>---.<+++++++++[>++++++<-]>+.<+++++++++[>++<-]>++++++.+++.------.-------*-.<,." nil)
+;; (brainfuck-interpret "+++++++++[>++++++++<-]>.<+++++++++[>+++<-]>++.+++++++..+++.<+++++++++[>-------<-]>----.<+++++++++[>-<-]>---.<+++++++++[>++++++<-]>+.<+++++++++[>++<-]>++++++.+++.------.--------.<+++++++++[>----------<-]>..<+++++++++[>++++++<-]>++.<+++++++++[>+++++<-]>+++.<+++++++++[>-<-]>--------.++++++++.+++++.--------.<+++++++++[>+<-]>++++++.<+++++++++[>--<-]>.++++++++.<+++++++++[>--------<-]>---.<+++++++++[>+++++<-]>+++++++.<+++++++++[>+<-]>++++++++.<+++++++++[>++<-]>+.----.<+++++++++[>---------<-]>---.<+++++++++[>++++<-]>+.<+++++++++[>+++<-]>++++.+++++.<+++++++++[>+<-]>++.-----.+++." nil)
 
-(provide 'brainfuck-mode)
+(add-to-list 'auto-mode-alist '("\\.bf" . brainfuck-mode))
+
+(provide 'brainfuck-Mode)
